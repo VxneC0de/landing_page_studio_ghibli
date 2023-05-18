3e8cf4c3a4b9623593d5df3af97891476f63e705
Cxr0l <vanerubio2020@gmail.com>
2023-05-17 20:00:17 -0400

    se acomodo el estilo del contenedor de la lista del menu hamburguesa

--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,14 @@
-let boton = document.getElementById("options");
-let listMovil = document.getElementById("menu-movil");
-console.log(boton);
+let boton = document.querySelector(".options");
+let listMovil = document.querySelector(".menu-movil");
 
 boton.addEventListener("click", () => {
     boton.classList.toggle("close");
     listMovil.classList.toggle("show");
-    console.log(listMovil);
-    })
+});
 
 listMovil.querySelectorAll("a").forEach((enlace) => {
     enlace.addEventListener("click", () => {
-      
-    listMovil.classList.remove("show");
-    boton.classList.remove("close");
+        listMovil.classList.remove("show");
+        boton.classList.remove("close");
     });
 });
